be046e969148e7747f4ea0d988f48a313dd9eb09
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-01-18 19:29:56 +0500

    hw08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,25 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+export const homeWorkReducer = (
+	state: UserType[],
+	action: ActionType
+): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case 'sort': {
+			// by name
+			return action.payload === 'up'
+				? [...state].sort((a, b) => a.name.localeCompare(b.name))
+				: [...state].sort((a, b) => b.name.localeCompare(a.name))
+		}
+		case 'check': {
+			return state.filter(el => el.age >= 18) // need to fix
+		}
+		default:
+			return state
+	}
 }
