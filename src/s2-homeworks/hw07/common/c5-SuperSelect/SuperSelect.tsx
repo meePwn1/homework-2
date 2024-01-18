a5be134fe888aa49f39fe67186dc2be0fa527c76
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-01-18 12:43:48 +0500

    fix bug hw07

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 	const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
 		// делают студенты
 
-		onChangeOption?.(e.target.value)
+		onChangeOption?.(Number(e.target.value))
 	}
 
 	const finalSelectClassName = s.select + (className ? ' ' + className : '')
