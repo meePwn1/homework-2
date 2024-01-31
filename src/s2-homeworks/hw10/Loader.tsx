2aeca9a1248fa115178c304b3fea4cad09e4dc91
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-01-31 19:36:51 +0500

    hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,8 @@
+import loader from '../../../public/Loading.png'
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+	<div className={s.loader}>
+		<img src={loader} alt='loader' />
+	</div>
+)
