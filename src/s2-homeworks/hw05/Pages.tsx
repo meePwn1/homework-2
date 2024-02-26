ba03e3fb8bbef37a504bddecd9f147ed37e606ca
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-02-26 19:57:58 +0500

    hw15

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -5,35 +5,35 @@ import JuniorPlus from './pages/JuniorPlus'
 import PreJunior from './pages/PreJunior'
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+	PRE_JUNIOR: '/pre-junior',
+	JUNIOR: '/junior',
+	JUNIOR_PLUS: '/junior-plus',
 }
 
 function Pages() {
-    return (
-        <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            <Routes>
-                <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR} replace />} />
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
-                <Route path={PATH.JUNIOR} element={<Junior />} />
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
-                <Route path='*' element={<Error404 />} />
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+	return (
+		<div>
+			{/*Routes выбирает первый подходящий роут*/}
+			<Routes>
+				<Route path='/' element={<Navigate to={PATH.PRE_JUNIOR} replace />} />
+				<Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+				<Route path={PATH.JUNIOR} element={<Junior />} />
+				<Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
+				<Route path='*' element={<Error404 />} />
+				{/*роутинг будут писать студенты*/}
+				{/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+				{/*<Route ...*/}
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+				{/*роуты для /pre-junior, /junior, /junior-plus*/}
+				{/*<Route ...*/}
+				{/*<Route ...*/}
+				{/*<Route ...*/}
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
-            </Routes>
-        </div>
-    )
+				{/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+				{/*<Route ...*/}
+			</Routes>
+		</div>
+	)
 }
 
 export default Pages
--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,54 +1,47 @@
 import React from 'react'
+import downSvg from '../assets/down.svg'
+import upDownSvg from '../assets/up-down.svg'
+import upSvg from '../assets/up.svg'
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const downIcon = downSvg
+const upIcon = upSvg
+const noneIcon = upDownSvg
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
+	id?: string
+	sort: string
+	value: string
+	onChange: (newSort: string) => void
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+	// пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+	switch (sort) {
+		case '':
+			return up
+		case up:
+			return down
+		default:
+			return ''
+	}
 }
 
-const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
-) => {
-    const up = '0' + value
-    const down = '1' + value
+const SuperSort: React.FC<SuperSortPropsType> = ({ sort, value, onChange, id = 'hw15' }) => {
+	const up = '0' + value
+	const down = '1' + value
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+	const onChangeCallback = () => {
+		onChange(pureChange(sort, down, up))
+	}
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+	const icon = sort === down ? downIcon : sort === up ? upIcon : noneIcon
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
-
-            {icon} {/*а это убрать*/}
-        </span>
-    )
+	return (
+		<span id={id + '-sort-' + value} onClick={onChangeCallback}>
+			<img id={id + '-icon-' + sort} src={icon} />
+		</span>
+	)
 }
 
 export default SuperSort
